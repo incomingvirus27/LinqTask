3b617aeba88032d101401673c87ca20e1ac043ed
Nwosu Yahgozie <gozmegatron@gmail.com>
2020-05-09 11:24:34 +0100

    recommit

---
+++ LabTask16/Article.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabTask16
+{
+    class Article
+    {
+        //Autoimplemented properties
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+        //constructor
+
+        public Article(int id, int categoryId, string title, string body)
+        {
+            Id = id;
+            CategoryId = categoryId;
+            Title = title;
+            Body = body;
+        }
+    }
+}
---
+++ LabTask16/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabTask16
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+      
+
+        public Category(int id, string categoryName)
+        {
+            Id = id;
+            CategoryName = categoryName;
+        }
+    }
+}
